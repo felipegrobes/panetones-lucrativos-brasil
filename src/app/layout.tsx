c0e1ab5d5c28845f0d9dbc0ae54beeeda0e80f57
Digital Space X <digitalspacex.ofc@gmail.com>
2025-11-10 22:00:32 +0000

    tire o codigo do pixel atual do facebook e adicione o novo codigo do pix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,13 +38,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '827917663128941');
+            fbq('init', '683636158144975');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=827917663128941&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=683636158144975&ev=PageView&noscript=1"
           />
         </noscript>
         <main className="font-body antialiased">
